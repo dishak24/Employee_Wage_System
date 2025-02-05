8e30eeb823bea97f707876b68880310f143614dd
Disha <dishakamble24@gmail.com>
2025-02-05 17:12:30 +0530

    UC1

--- Program.cs
+++ Program.cs
@@ -9,6 +9,33 @@ namespace UseCaseCode
 
             Console.WriteLine("Welcome to Employee Wages System !");
 
-        }
+            while (true)
+            {
+                EmployeeWage employeeWage = new EmployeeWage(); 
+                Console.WriteLine("1: Attendance Checking");
+                Console.WriteLine("2: Calculate Employees Daily Wages");
+                Console.WriteLine("3: Calculate Part time Employee Wages");
+                Console.WriteLine("4: Calculate wages for a Month");
+                Console.WriteLine("5: calculate wage till a condition of total working hours or days is reached for a month");
+                Console.WriteLine("0: Exit");
+                Console.WriteLine();
+                Console.WriteLine("Enter your Choice :");
+                int ch = Convert.ToInt32(Console.ReadLine());
+
+                switch (ch)
+                {
+                    case 1:
+                        employeeWage.AttendanceChecker();
+                        break;
+
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Choice!");
+                        break;
+
+                }
     }
 }
