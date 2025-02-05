2f2b4f8847a6f1a885fb74694dba82dff39f01a5
Disha <dishakamble24@gmail.com>
2025-02-05 16:53:34 +0530

    Main Method

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UseCaseCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Employee Wages System !");
+
+        }
+    }
+}
