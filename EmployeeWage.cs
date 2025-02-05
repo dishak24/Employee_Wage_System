853e7be912aed72619cb07013b3aa52aade395d2
Disha <dishakamble24@gmail.com>
2025-02-05 17:17:03 +0530

    UC1

---
+++ EmployeeWage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UseCaseCode
+{
+    internal class EmployeeWage
+    {
+        public void AttendanceChecker()
+        {
+            Random r = new Random();
+            int attendance = r.Next(0, 2);
+            if (attendance == 1)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+
+    }
+}
